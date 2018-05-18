d15fd453b64031150ad297026456c7bacc59851b
Krzysztof Płocharz <plocharz@9livesdata.com>
2018-05-18 15:41:10 +0200

    Make internal/health endpoint available in api

--- src/sentry/api/endpoints/system_health.py
+++ src/sentry/api/endpoints/system_health.py
@@ -7,12 +7,12 @@ from rest_framework.response import Response
 from sentry import status_checks
 from sentry.status_checks import sort_by_severity
 from sentry.api.base import Endpoint
-from sentry.api.permissions import SuperuserPermission
+from sentry.api.bases.organization import OrganizationPermission
 from sentry.utils.hashlib import md5_text
 
 
 class SystemHealthEndpoint(Endpoint):
-    permission_classes = (SuperuserPermission, )
+    permission_classes = (OrganizationPermission, )
 
     def get(self, request):
         results = status_checks.check_all()
